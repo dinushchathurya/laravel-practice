d1cf6189dc1edf9492a90fd0d1de8560fb9606aa
dinushchathurya <dinushchathurya21@gmail.com>
2022-02-26 16:21:28 +0530

    feat: Create function for get all users

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -3,24 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Repositories\CustomerRepository;
 use App\Models\Customer;
 
 class CustomerController extends Controller
-{
+{   
+
+    private $customerRepository;
+
+    public function __construct(CustomerRepository $customerRepository)
+    {
+        $this->customerRepository = $customerRepository;
+    }
+
     public function index()
     {
-        $customers = Customer::where('active', 1)
-        ->with('user')
-        ->get()
-        ->map(function ($customer) {
-            return [
-                'customer_id' => $customer->id,
-                'name' => $customer->name,
-                'created_by' => $customer->user->email,
-                'updated_at' => $customer->updated_at->diffforhumans(),
-            ];
-        });
-
+        $customers = $this->customerRepository->getAll();
         return $customers;
     }
 }
